be9d4096e4ad2b6907638119c55a4a4c2b6cc6a1
Abdugani Abduqaxxorov <83403180+abduganii@users.noreply.github.com>
2022-03-23 06:16:53 +0500

    ok
    change

--- src/server.js
+++ src/server.js
@@ -1,16 +1,19 @@
 const express = require('express')
 const app = express()
 const { ApolloServer } = require('apollo-server-express')
+const PORT = process.env.PORT || 5000
 
 const modules=require('./modules')
 
 const server = new ApolloServer({
-    modules
+    modules,
+    introspection: true,
+    playground:true
 })
 
 server.applyMiddleware({app})
 
-app.listen({ port: 5000 }, () => {
+app.listen({ port: PORT }, () => {
     console.log('http://localhost:5000'+server.graphqlPath)
     console.log('ws://localhost:5000'+server.subscriptionsPath)
 })
